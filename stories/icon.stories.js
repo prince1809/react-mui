ff7c258be4f1f631fc465be16353f326a378e1f9
prince <prince18pat@gmail.com>
2019-06-21 02:21:33 +0900

    add styling to stories

--- stories/icon.stories.js
+++ stories/icon.stories.js
@@ -3,6 +3,17 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Icon } from '../src';
+import { withStyles } from '@material-ui/styles';
+
+const SimpleIcons = withStyles(
+  theme => ({
+    icon: {
+      margin: 24,
+    },
+  })
+)(({ classes }) => (
+  <Icon className={classes.icon}>star</Icon>
+));
 
 storiesOf('Icon', module)
-  .add('with text', () => <Icon>star</Icon>);
+  .add('with text', () => <SimpleIcons />);
