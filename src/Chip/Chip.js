1bdb8bd512b5d9b7924f9ebe7c3bc5afbe0513d4
prince <prince18pat@gmail.com>
2019-06-24 01:58:43 +0900

    add chips styling

--- src/Chip/Chip.js
+++ src/Chip/Chip.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '../styles/withStyles';
+import clsx from 'clsx';
+import { capitalize } from '../utils/helpers';
 
 export const styles = theme => {
 
@@ -13,12 +15,53 @@ export const styles = theme => {
 
 const Chip = React.forwardRef(function Chip(props, ref) {
   const {
+    avatar: avatarProp,
+    classes,
+    className: classNameProp,
+    clickable: clickableProp,
+    color = 'default',
     component: Component = 'div',
+    deleteIcon: deleteIconProp,
+    icon: iconProp,
+    label,
+    onClick,
+    onDelete,
+    onKeyDown,
+    onKeyUp,
+    size = 'medium',
+    variant = 'default',
+    ...other
   } = props;
 
+  const chipRef = React.useRef(null);
+
+  const handleDeleteIconClick = event => {
+    event.stopPropagation();
+    if (onDelete) {
+      onDelete(event);
+    }
+  }
+
+  const small = size === 'small';
+
+  let avatar = null;
+  if (avatarProp && React.isValidElement(avatarProp)) {
+    avatar = React.cloneElement(avatarProp, {
+      className: clsx(classes.avatar, avatarProp.props.className, {
+        [classes.avatarSmall]: small,
+        [classes[`avatarColor${capitalize(color)}`]]: color !== 'default',
+      }),
+      childrenClassName: clsx(classes.avatarChildren, avatarProp.props.childrenClassName),
+    });
+  }
+
+  let icon = null;
+
+
   return (
-    <Component>
-      Chip
+    <Component
+    >
+      {avatar || icon}
     </Component>
   );
 });
