5dabef5c48066a7a04a33cc2b6da4e894faf3b95
prince <prince18pat@gmail.com>
2019-06-30 07:22:56 +0900

    add toolbar styling

--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -4,7 +4,23 @@ import clsx from 'clsx';
 import withStyles from '../styles/withStyles';
 
 export const styles = theme => ({
-
+  root: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  gutters: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      paddingLeft: theme.spacing(3),
+      paddingRight: theme.spacing(3),
+    }
+  },
+  regular: theme.mixins.toolbar,
+  dense: {
+    minHeight: 48,
+  }
 });
 
 const Toolbar = React.forwardRef(function Toolbar(props, ref) {
@@ -17,10 +33,25 @@ const Toolbar = React.forwardRef(function Toolbar(props, ref) {
     ...other
   } = props;
 
-  return <Component {...other} />;
+  const className = clsx(
+    classes.root,
+    classes[variant],
+    {
+      [classes.gutters]: !disableGutters,
+    },
+    classNameProp,
+  );
+
+  return <Component className={className} ref={ref} {...other} />;
 });
 
 Toolbar.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  component: PropTypes.elementType,
+  disableGutters: PropTypes.bool,
+  variant: PropTypes.oneOf(['regular', 'dense']),
 };
 
 export default withStyles(styles, { name: 'MuiToolbar' })(Toolbar);
