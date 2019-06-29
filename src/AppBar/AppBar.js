0679123da8dd36102eb64fd00c047d53cb0ba81f
prince <prince18pat@gmail.com>
2019-06-30 07:10:57 +0900

    fix appbar styling

--- src/AppBar/AppBar.js
+++ src/AppBar/AppBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import Paper from '../Paper';
 import withStyles from '../styles/withStyles';
+import { capitalize } from '../utils/helpers';
 
 export const styles = theme => {
   const backgroundColorDefault = theme.palette.type == 'light' ? theme.palette.grey[100] : theme.palette.grey[900];
@@ -10,8 +11,43 @@ export const styles = theme => {
   return {
     root: {
       display: 'flex',
-    }
-  }
+      flexDirection: 'column',
+      width: '100%',
+      boxSizing: 'border-box',
+      zIndex: theme.zIndex.appBar,
+      flexShrink: 0,
+    },
+    positionFixed: {
+      position: 'fixed',
+      top: 0,
+      left: 'auto',
+      right: 0,
+    },
+    positionAbsolute: {
+
+    },
+    positionSticky: {
+
+    },
+    positionStatic: {
+      position: 'static',
+    },
+    positionRelative: {
+
+    },
+    colorDefault: {
+      backgroundColor: backgroundColorDefault,
+      color: theme.palette.getContrastText(backgroundColorDefault),
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+    },
+    colorSecondary: {
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText,
+    },
+  };
 };
 
 const AppBar = React.forwardRef(function AppBar(props, ref) {
@@ -19,13 +55,30 @@ const AppBar = React.forwardRef(function AppBar(props, ref) {
 
   return (
     <Paper
+      square
+      component="header"
+      elevation={4}
+      className={clsx(
+        classes.root,
+        classes[`position${capitalize(position)}`],
+        {
+          [classes[`color${capitalize(color)}`]]: color !== 'inherit',
+          'mui-fixed': position === 'fixed', // Useful for the Dialog
+        },
+        className,
+      )}
+      ref={ref}
       {...other}
     />
   );
 });
 
 AppBar.prpoTypes = {
-
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  color: PropTypes.oneOf(['inherit', 'primary', 'secondary', 'default']),
+  position: PropTypes.oneOf(['fixed', 'absolute', 'sticky', 'static', 'relative']),
 };
 
 export default withStyles(styles, { name: 'MuiAppBar' })(AppBar);
--- stories/appbar.stories.js
+++ stories/appbar.stories.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { muiTheme } from 'storybook-addon-material-ui';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import withStyles from '../src/styles/withStyles';
 import AppBar from '../src/AppBar';
+import Toolbar from '../src/Toolbar';
+import IconButton from '../src/IconButton';
+import Typography from '../src/Typography';
+import Button from '../src/Button';
 
 
 
 const DefaultChips = withStyles(
   theme => ({
     root: {
-
+      flexGrow: 1,
     },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    }
   }),
 )(({ classes }) => (
   <div className={classes.root}>
-    <AppBar>
-      Appbar
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
     </AppBar>
   </div>
 ))
